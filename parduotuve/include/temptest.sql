dc62ffbea2fb54fa88a117aad912cbe89584b2e1
galnereikia <92311958+galnereikia@users.noreply.github.com>
2023-12-19 05:30:41 +0200

    Update temptest.sql

--- parduotuve/include/temptest.sql
+++ parduotuve/include/temptest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2023 at 09:21 PM
+-- Generation Time: Dec 19, 2023 at 04:26 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -40,8 +40,7 @@ CREATE TABLE `administratoriai` (
 --
 
 INSERT INTO `administratoriai` (`idarbinimo_data`, `Tel_nr`, `Alga`, `id_Administratorius`, `fk_Naudotojasid_Naudotojas`) VALUES
-('2023-12-13', '48448464546', 2000, 3, 9),
-('2023-12-12', '516515', 2000, 4, 12);
+('2023-12-18', '862154637', 0, 5, 16);
 
 -- --------------------------------------------------------
 
@@ -70,7 +69,13 @@ INSERT INTO `adresai` (`Miestas`, `Salis`, `Pasto_kodas`, `Gatve`, `Namo_nr`, `i
 ('Londonas', 'DB', '5646', 'maj', 5, 5, 12),
 ('Maskva', 'Rusija', '5646', 'Lokulu', 5, 6, 13),
 ('Vilnius', 'Lietuva', '5646', 'Lokulu', 5, 7, 14),
-('Kaunas', 'Lietuva', '5646', 'Lokulu', 5, 8, 15);
+('Kaunas', 'Lietuva', '5646', 'Lokulu', 5, 8, 15),
+('Kaunas', 'Lietuva', '70317', 'Aguonų g.', 6, 9, 16),
+('Vilnius', 'Lietuva', '35538', 'Aido g.', 8, 10, 17),
+('Marijampolė', 'Lietuva', '53535', 'Akmenės g.', 1, 11, 18),
+('Klaipėda', 'Vokietija', '75757', 'Alyvų g.', 15, 12, 19),
+('Vilnius', 'Lietuva', '58337', 'Alyvų g.', 20, 13, 20),
+('test', 'test', '2422', 'test', 1, 14, 21);
 
 -- --------------------------------------------------------
 
@@ -101,7 +106,7 @@ INSERT INTO `apeliacijos` (`tekstas`, `priezastis`, `data`, `id_Apeliacija`, `fk
 
 CREATE TABLE `kategorijos` (
   `id` int(11) NOT NULL,
-  `pavadinimas` varchar(56) NOT NULL,
+  `kategorijos_pavadinimas` varchar(56) NOT NULL,
   `prekiu_sk` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -109,7 +114,7 @@ CREATE TABLE `kategorijos` (
 -- Dumping data for table `kategorijos`
 --
 
-INSERT INTO `kategorijos` (`id`, `pavadinimas`, `prekiu_sk`) VALUES
+INSERT INTO `kategorijos` (`id`, `kategorijos_pavadinimas`, `prekiu_sk`) VALUES
 (1, 'kita', NULL),
 (3, 'kompiuterių priedai', NULL),
 (5, 'kompiuteriai', NULL);
@@ -126,17 +131,21 @@ CREATE TABLE `komentarai` (
   `laikas` varchar(255) NOT NULL,
   `id_Komentaras` int(11) NOT NULL,
   `fk_Prekeid_Preke` int(11) NOT NULL,
-  `fk_Pirkejasid_Pirkejas` int(11) NOT NULL
+  `fk_Pirkejasid_Pirkejas` int(11) NOT NULL,
+  `parent_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `komentarai`
 --
 
-INSERT INTO `komentarai` (`tekstas`, `data`, `laikas`, `id_Komentaras`, `fk_Prekeid_Preke`, `fk_Pirkejasid_Pirkejas`) VALUES
-('komentaras', '2023-12-14', '16:00', 2, 3, 3),
-('gera preke', '0000-00-00', '15:25', 3, 6, 5),
-('gera preke', '2023-05-12', '15:25', 4, 6, 5);
+INSERT INTO `komentarai` (`tekstas`, `data`, `laikas`, `id_Komentaras`, `fk_Prekeid_Preke`, `fk_Pirkejasid_Pirkejas`, `parent_id`) VALUES
+('komentaras', '2023-12-14', '16:00', 2, 3, 3, NULL),
+('gera preke', '0000-00-00', '15:25', 3, 6, 5, NULL),
+('gera preke', '2023-05-12', '15:25', 4, 6, 5, NULL),
+('test', '2023-12-18', '19:35:11', 5, 5, 7, NULL),
+('Netyčia', '2023-12-18', '19:35:23', 6, 5, 7, 5),
+('Gera prekė', '2023-12-18', '19:39:36', 7, 7, 7, NULL);
 
 -- --------------------------------------------------------
 
@@ -160,13 +169,18 @@ CREATE TABLE `naudotojai` (
 
 INSERT INTO `naudotojai` (`Vardas`, `Pavarde`, `El_pastas`, `Slaptazodis`, `Ar_blokuotas`, `Naudotojo_lygis`, `id_Naudotojas`) VALUES
 ('aaaaaaaaaa', 'aaaaaaaaa', 'nrr@gmail.com', '$2y$10$ndwijoHfAr1smsjaPakrned8yan2nhWpL9A4uYbpwc8y2Dj7AwM.a', 1, 3, 8),
-('admin', 'aaaaaaaaa', 'f@gmail.com', '$2y$10$fBpLtSYKuYCFCss1B1kyFeHip4rXzqtnxGlmgsOToz.z8h7.hKwUC', 0, 1, 9),
+('admin', 'aaaaaaaaa', 'f@gmail.com', '$2y$10$fBpLtSYKuYCFCss1B1kyFeHip4rXzqtnxGlmgsOToz.z8h7.hKwUC', 0, 0, 9),
 ('naudotojas', 'n', 'naud@gmail.com', '$2y$10$O1RYOmRKzUXZuWwwCJhzwOmfoAj/9lvCBuHP/v.AjmYbxJY2lrh6q', 0, 3, 11),
-('administratorius', 'aa', 'ad@gmail.com', '$2y$10$03DXa4Btk7EeBPSlg99n8eoOYwg12vp/A53XD0Y1DFrDQji.Ytrka', 0, 1, 12),
+('administratorius', 'aa', 'rasdsadas@gmail.com', '$2y$10$03DXa4Btk7EeBPSlg99n8eoOYwg12vp/A53XD0Y1DFrDQji.Ytrka', 0, 0, 12),
 ('pardavejas', 'par', 'pr@gmail.com', '$2y$10$SnQ2qUvI3Rc92IPrEi3jduHx3U3tqodTj67MXuf7lzqSKk8PuJtKO', 0, 2, 13),
 ('nejus', 'netiejus', 'netej@gmail.com', '$2y$10$reyetV3DBABV5aI66DD5nu6mYodHWzO6fA7o8Cilm1R4lIa2uo1va', 0, 3, 14),
 ('pranas', 'pedantas', 'pr@gmail.com', '$2y$10$2jWII2qXlx9saHuFduNRJuSQ.5dux.UIAMn02ucF.zOmEtra7Tg6q', 0, 2, 15),
-('test', 'test', 'test@test.com', '$2y$10$f8ykqNYjaAhyDdiS3/W4oe6s8uoq7K4yCWEtSzkn/ZBl7atTV1baS', 0, 3, 17);
+('owner', 'renwo', 'parduotuve.email@gmail.com', '$2y$10$bS7O5FuADJBa5kFPHHY6OuWjOjdx3JRHOaAZLLynajN8dSPkfgpIG', 0, 1, 16),
+('matas', 'satam', 'test@gmail.com', '$2y$10$wCIxx.2Vk4kce/P/p/mUoegwbXG/tFYqQnXRfgluiAZf7FiU8jY.m', 0, 2, 17),
+('tadas', 'sadat', 'paprastas@gmail.com', '$2y$10$ndGKZXD1.93gICKHnJVJxuUKSiDUwgL2w1El4x2AEIbWpIMnRjoGW', 0, 3, 18),
+('ignas', 'sangi', 'Negalima@gmail.com', '$2y$10$rAr0F96OOhZMRVcNATOUr.KKsyRwDJNgnmPJcYdKYE9034uIC/HZG', 0, 2, 19),
+('petras', 'sartep', 'Galimas@gmail.com', '$2y$10$a4FogKvZopjR60NuSQWDHOk8kU2sAj5LWLYhv982W7.cO70QrbhhO', 0, 3, 20),
+('test', 'test', 'testas@gmail.com', '$2y$10$WShkE8kudjuxOOHnE/jmweHUu5uD7FwKmPQINR6CZatERpCCHY3g.', 0, 0, 21);
 
 -- --------------------------------------------------------
 
@@ -189,7 +203,9 @@ CREATE TABLE `pardavejai` (
 
 INSERT INTO `pardavejai` (`Ar_patvirtintas`, `patvirtinimo_data`, `Ikeltu_prekiu_skaicius`, `vertinimu_vidurkis`, `id_Pardavejas`, `fk_Naudotojasid_Naudotojas`) VALUES
 (0, NULL, 0, NULL, 4, 15),
-(1, '2023-12-12', 5, 3, 5, 13);
+(1, '2023-12-12', 5, 3, 5, 13),
+(0, NULL, 0, NULL, 6, 17),
+(1, NULL, 0, NULL, 7, 19);
 
 -- --------------------------------------------------------
 
@@ -212,7 +228,9 @@ CREATE TABLE `pirkejai` (
 INSERT INTO `pirkejai` (`vertinimu_vidurkis`, `uzsakymu_skaicius`, `komentaru_skaicius`, `id_Pirkejas`, `fk_Naudotojasid_Naudotojas`) VALUES
 (NULL, 2, 2, 3, 8),
 (NULL, 0, 0, 4, 11),
-(NULL, 1, 5, 5, 14);
+(NULL, 1, 5, 5, 14),
+(NULL, 0, 0, 6, 18),
+(NULL, 0, 0, 7, 20);
 
 -- --------------------------------------------------------
 
@@ -250,7 +268,7 @@ INSERT INTO `pranesimai` (`data`, `gavejas`, `priezastis`, `tekstas`, `id_Pranes
 CREATE TABLE `prekes` (
   `pavadinimas` varchar(255) NOT NULL,
   `kaina` double NOT NULL,
-  `kategorija` int(11) NOT NULL,
+  `kategorija` varchar(255) NOT NULL,
   `gamintojas` varchar(255) DEFAULT NULL,
   `ar_paslepta` tinyint(1) NOT NULL,
   `id_Preke` int(11) NOT NULL,
@@ -262,12 +280,17 @@ CREATE TABLE `prekes` (
 --
 
 INSERT INTO `prekes` (`pavadinimas`, `kaina`, `kategorija`, `gamintojas`, `ar_paslepta`, `id_Preke`, `fk_Pardavėjasid_Pardavėjas`) VALUES
-('pav', 20.01, 1, 'gam', 0, 3, 5),
-('Prailginimo laidas', 5.51, 1, 'utex', 1, 4, 5),
-('Lygintuvas', 105.51, 1, 'utex', 0, 5, 5),
-('Krovejas', 5.51, 1, 'utex', 0, 6, 5),
-('Siurblys', 50.51, 1, 'utex', 0, 7, 5),
-('lenta', 10, 1, 'kita', 0, 8, 4);
+('pav', 20.01, 'kat', 'gam', 0, 3, 5),
+('Prailginimo laidas', 5.51, 'laidai', 'utex', 1, 4, 5),
+('Lygintuvas', 105.51, 'Namu prietasai', 'utex', 0, 5, 5),
+('Krovejas', 5.51, 'laidai', 'utex', 0, 6, 5),
+('Siurblys', 50.51, 'prietaisai', 'utex', 0, 7, 5),
+('testas', 2, '', 'testas', 0, 8, 6),
+('testas', 2, 'Kompiuteriai', 'et', 0, 10, 7),
+('fsdmfn', 12, 'default', 'fdf', 0, 11, 6),
+('fsdmfn', 12, 'default', 'fdf', 0, 12, 6),
+('fsdmfn', 12, 'default', 'fdf', 0, 13, 6),
+('ksvk', 20, 'default', 'dfnjd', 0, 14, 6);
 
 -- --------------------------------------------------------
 
@@ -278,12 +301,19 @@ INSERT INTO `prekes` (`pavadinimas`, `kaina`, `kategorija`, `gamintojas`, `ar_pa
 CREATE TABLE `uzsakymai` (
   `data` date NOT NULL,
   `uzsakymo_kaina` double NOT NULL,
-  `būsena` varchar(255) NOT NULL,
+  `busena` varchar(255) NOT NULL,
   `pristatymo_budas` varchar(255) NOT NULL,
   `id_Uzsakymas` int(11) NOT NULL,
   `fk_Pirkejasid_Pirkejas` int(11) NOT NULL,
   `fk_Administratoriusid_Administratorius` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `uzsakymai`
+--
+
+INSERT INTO `uzsakymai` (`data`, `uzsakymo_kaina`, `busena`, `pristatymo_budas`, `id_Uzsakymas`, `fk_Pirkejasid_Pirkejas`, `fk_Administratoriusid_Administratorius`) VALUES
+('2023-12-04', 203.4, 'Vykdoma', 'Paštomatas', 2, 7, NULL);
 
 -- --------------------------------------------------------
 
@@ -297,6 +327,13 @@ CREATE TABLE `uzsakymo_prekes` (
   `fk_Uzsakymasid_Uzsakymas` int(11) NOT NULL,
   `fk_Prekeid_Preke` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `uzsakymo_prekes`
+--
+
+INSERT INTO `uzsakymo_prekes` (`kiekis`, `id_Uzsakymo_prekė`, `fk_Uzsakymasid_Uzsakymas`, `fk_Prekeid_Preke`) VALUES
+(2, 3, 2, 8);
 
 -- --------------------------------------------------------
 
@@ -383,8 +420,7 @@ ALTER TABLE `pranesimai`
 --
 ALTER TABLE `prekes`
   ADD PRIMARY KEY (`id_Preke`),
-  ADD KEY `Parduoda` (`fk_Pardavėjasid_Pardavėjas`),
-  ADD KEY `fk_kategorija` (`kategorija`);
+  ADD KEY `Parduoda` (`fk_Pardavėjasid_Pardavėjas`);
 
 --
 -- Indexes for table `uzsakymai`
@@ -418,13 +454,13 @@ ALTER TABLE `vertinimai`
 -- AUTO_INCREMENT for table `administratoriai`
 --
 ALTER TABLE `administratoriai`
-  MODIFY `id_Administratorius` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_Administratorius` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `adresai`
 --
 ALTER TABLE `adresai`
-  MODIFY `id_Adresas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_Adresas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `apeliacijos`
@@ -442,25 +478,25 @@ ALTER TABLE `kategorijos`
 -- AUTO_INCREMENT for table `komentarai`
 --
 ALTER TABLE `komentarai`
-  MODIFY `id_Komentaras` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_Komentaras` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `naudotojai`
 --
 ALTER TABLE `naudotojai`
-  MODIFY `id_Naudotojas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_Naudotojas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `pardavejai`
 --
 ALTER TABLE `pardavejai`
-  MODIFY `id_Pardavejas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_Pardavejas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `pirkejai`
 --
 ALTER TABLE `pirkejai`
-  MODIFY `id_Pirkejas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_Pirkejas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `pranesimai`
@@ -472,19 +508,19 @@ ALTER TABLE `pranesimai`
 -- AUTO_INCREMENT for table `prekes`
 --
 ALTER TABLE `prekes`
-  MODIFY `id_Preke` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_Preke` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `uzsakymai`
 --
 ALTER TABLE `uzsakymai`
-  MODIFY `id_Uzsakymas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_Uzsakymas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `uzsakymo_prekes`
 --
 ALTER TABLE `uzsakymo_prekes`
-  MODIFY `id_Uzsakymo_prekė` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_Uzsakymo_prekė` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `vertinimai`
@@ -543,8 +579,7 @@ ALTER TABLE `pranesimai`
 -- Constraints for table `prekes`
 --
 ALTER TABLE `prekes`
-  ADD CONSTRAINT `Parduoda` FOREIGN KEY (`fk_Pardavėjasid_Pardavėjas`) REFERENCES `pardavejai` (`id_Pardavejas`),
-  ADD CONSTRAINT `fk_kategorija` FOREIGN KEY (`kategorija`) REFERENCES `kategorijos` (`id`);
+  ADD CONSTRAINT `Parduoda` FOREIGN KEY (`fk_Pardavėjasid_Pardavėjas`) REFERENCES `pardavejai` (`id_Pardavejas`);
 
 --
 -- Constraints for table `uzsakymai`
